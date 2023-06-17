c820c3288fa3f3fa36f70bb1c65f39db68d8a7c7
Oghenekaro <karonstical@gmail.com>
2023-06-17 03:09:04 +0100

    sail we go

---
+++ datatype.py
@@ -0,0 +1,11 @@
+plum = 25
+many = str("Girls")
+TheyEatFood = bool("Yes")
+#print(type(plum))
+#many = str("Girls")
+#print(type(many))
+#TheyEatFood = bool("Yes")
+print(type(plum), type(many), type(TheyEatFood))
+
+
+
---
+++ love.py
@@ -0,0 +1,9 @@
+first_name = "OGHENEKARO" 
+
+middle_name = "CLETUS" 
+
+last_name = "ANAKPOHA" 
+
+full_name = first_name + " " + middle_name + " " + last_name 
+
+print("The first name of the user is {}. The middle name of the user is {}. The last name of the user is {}." .format(first_name, middle_name, last_name))  
